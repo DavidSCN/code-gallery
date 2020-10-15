313e20847ee9ac6ae75710a92fb72b44a04580b5
D Schneider <dav.schneider@tum.de>
2020-10-15 12:41:24 +0200

    Apply review changes in dummy participant and config

--- coupled_laplace_problem/dummy.cc
+++ coupled_laplace_problem/dummy.cc
@@ -19,22 +19,17 @@ define_boundary_values(std::vector<double> &boundary_data, double amplitude)
 int
 main()
 {
+  // Adjust to MPI rank and size for parallel computation
   const int commRank = 0;
   const int commSize = 1;
 
   using namespace precice;
-  using namespace precice::constants;
 
   // Configuration
   const std::string configFileName("precice-config.xml");
   const std::string solverName("dummy-participant");
   const std::string meshName("dummy-mesh");
   const std::string dataWriteName("boundary-data");
-  const std::string dataReadName("dummy");
-
-  std::cout << "DUMMY: Running solver dummy with preCICE config file \""
-            << configFileName << "\", participant name \"" << solverName
-            << "\", and mesh name \"" << meshName << "\".\n";
 
   SolverInterface precice(solverName, configFileName, commRank, commSize);
 
@@ -43,21 +38,17 @@ main()
   const int numberOfVertices = 6;
 
 
-  const int readDataID = precice.hasData(dataReadName, meshID) ?
-                           precice.getDataID(dataReadName, meshID) :
-                           -1;
-  const int writeDataID = precice.hasData(dataWriteName, meshID) ?
-                            precice.getDataID(dataWriteName, meshID) :
-                            -1;
+  const int dataID = precice.getDataID(dataWriteName, meshID);
 
   // Set up data structures
-  std::vector<double> readData(numberOfVertices);
   std::vector<double> writeData(numberOfVertices);
   std::vector<int>    vertexIDs(numberOfVertices);
   std::vector<double> vertices(numberOfVertices * dimensions);
 
   // Define a boundary mesh
-  const double deltaX = 2. / (numberOfVertices - 1);
+  const double length  = 2;
+  const double x_coord = 1;
+  const double deltaX  = length / (numberOfVertices - 1);
   for (int i = 0; i < numberOfVertices; ++i)
     for (int j = 0; j < dimensions; ++j)
       {
@@ -65,7 +56,7 @@ main()
         // The x-coordinate is always 1, i.e., the boundary is parallel to the
         // y-axis. The y-coordinate is descending from 1 to -1.
         if (j == 0)
-          vertices[index] = 1;
+          vertices[index] = x_coord;
         else
           vertices[index] = 1 - deltaX * i;
       }
@@ -82,15 +73,15 @@ main()
   // initialize the Solverinterface
   double dt = precice.initialize();
 
-  if (precice.isActionRequired(actionWriteInitialData()))
+  if (precice.isActionRequired(constants::actionWriteInitialData()))
     {
       std::cout << "DUMMY: Writing initial data \n";
-      precice.writeBlockScalarData(writeDataID,
+      precice.writeBlockScalarData(dataID,
                                    numberOfVertices,
                                    vertexIDs.data(),
                                    writeData.data());
 
-      precice.markActionFulfilled(actionWriteInitialData());
+      precice.markActionFulfilled(constants::actionWriteInitialData());
 
       precice.initializeData();
     }
@@ -105,7 +96,7 @@ main()
       if (precice.isWriteDataRequired(dt))
         {
           std::cout << "DUMMY: Writing coupling data \n";
-          precice.writeBlockScalarData(writeDataID,
+          precice.writeBlockScalarData(dataID,
                                        numberOfVertices,
                                        vertexIDs.data(),
                                        writeData.data());
@@ -113,16 +104,6 @@ main()
 
       dt = precice.advance(dt);
       std::cout << "DUMMY: Advancing in time\n";
-
-      // FIXME: Should retun false
-      if (precice.isReadDataAvailable() && false)
-        {
-          std::cout << "DUMMY: Reading coupling data \n";
-          precice.readBlockScalarData(readDataID,
-                                      numberOfVertices,
-                                      vertexIDs.data(),
-                                      readData.data());
-        }
     }
 
   std::cout << "DUMMY: Closing C++ solver dummy...\n";
